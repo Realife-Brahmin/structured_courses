3daddccc7765b018604f4ab25311e60f5a89ba7f
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-09-24 14:00:00 -0700

    lol C of ABCD is conflicting with C of capacitor, so now making the latter as C_cap

--- ee582/main.m
+++ ee582/main.m
@@ -3,18 +3,18 @@ addpath(genpath(thisFolder));
 
 rawDataFolder = "./rawData/";
 
-r_L = 1; L = 19e-3; C = 8.2e-6;
-w_n = 1/sqrt(L*C)
-zeta = 1/2*r_L/(sqrt(L/C))
+r_L = 1; L = 19e-3; C_cap = 8.2e-6;
+w_n = 1/sqrt(L*C_cap)
+zeta = 1/2*r_L/(sqrt(L/C_cap))
 w_d = w_n*sqrt(1-zeta^2)
-lambda = (-r_L/L + sqrt((r_L/L)^2 - 4/(L*C)))/2
+lambda = (-r_L/L + sqrt((r_L/L)^2 - 4/(L*C_cap)))/2
 Qfactor = w_n*L/r_L
 V_LL = 13.8e3;
 V_ph = V_LL/sqrt(3);
 f_fun = 60;
 w_fun = 2*pi*f_fun;
 X_L = w_fun*L;
-X_C = 1/(w_fun*C);
+X_C = 1/(w_fun*C_cap);
 X = X_L - X_C;
 Z = r_L + 1j*X
 I_ph = V_ph/Z
