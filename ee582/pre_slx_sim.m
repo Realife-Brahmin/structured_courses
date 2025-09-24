aa6b1e52cc3bca898130d1784e99b8230949ef23
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-09-24 13:59:13 -0700

    eigen values computed via matlab are also in agreement with theoretical ones

--- ee582/pre_slx_sim.m
+++ ee582/pre_slx_sim.m
@@ -8,6 +8,20 @@ model_file = fullfile(rawDataFolder, 'hw01_q03.slx');
 model_file = strrep(model_file, '\', '/');
 [~, model, ~] = fileparts(model_file);
 
+% Linearize the model to get state-space matrices
+% [~, ~, ~, ~, xnames] = linmod(model);
+% disp(xnames);
+[A, B, C, D, xnames] = linmod(model);
+stateNames = xnames.stateName;
+
+disp(stateNames);
+% Compute the eigenvalues of the A matrix
+format long g
+eigvals = eig(A);
+
+% Display the eigenvalues
+disp('Eigenvalues of the system:');
+disp(eigvals);
 % Set simulation start and stop time
 set_param(model, 'StartTime', '0.0');
 set_param(model, 'StopTime', num2str(T_horizon_s)); % T_horizon_s must be defined in workspace
@@ -16,6 +30,8 @@ set_param(model, 'StopTime', num2str(T_horizon_s)); % T_horizon_s must be define
 set_param(model, 'SolverType', 'Fixed-step'); % or 'Variable-step'
 set_param(model, 'Solver', 'ode3'); % 'ode3' for Bogacki-Shampine, 'ode45', etc.
 set_param(model, 'FixedStep', num2str(h_s)); % h_s must be defined in workspace
+
+
 
 % Save the model if you want to persist these changes
 % save_system(model);
