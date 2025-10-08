18ee6746e1ed600d05cefffdf0127365afaab686
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-10-07 20:03:20 -0700

    skel

---
+++ ee582/src_Hw02/hw02_qA1.m
@@ -0,0 +1,26 @@
+%% ===================== Global light theme =====================
+set(groot,'defaultFigureColor','w');
+set(groot,'defaultAxesColor','w');
+set(groot,'defaultAxesXColor','k');
+set(groot,'defaultAxesYColor','k');
+set(groot,'defaultAxesGridColor',[.2 .2 .2]);
+set(groot,'defaultAxesMinorGridColor',[.6 .6 .6]);
+set(groot,'defaultAxesFontName','Helvetica');
+set(groot,'defaultAxesFontSize',12);
+set(groot,'defaultAxesTitleFontWeight','bold');
+set(groot,'defaultAxesTitleFontSizeMultiplier',1.1);
+set(groot,'defaultAxesXMinorGrid','on');
+set(groot,'defaultAxesYMinorGrid','on');
+set(groot,'defaultLineLineWidth',1.4);
+set(groot,'defaultLineMarkerSize',9);
+set(groot, 'defaultLegendColor', 'w');      % White background for all legends
+set(groot, 'defaultLegendTextColor', 'k');  % Black text for all legends
+
+% --- Setup ---
+T_horizon_s = 1e-3;
+h_ms = 0.1;
+h_s = h_ms * 1e-3;
+h_mus = h_ms * 1e3;
+
+rawDataFolder = "./rawData/";
+
